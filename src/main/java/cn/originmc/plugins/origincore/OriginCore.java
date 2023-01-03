71c7d3619bb06d180bb8dd6bb1f722743e04a594
Yeqi99 <72844744+Yeqi99@users.noreply.github.com>
2023-01-04 04:05:48 +0800

    更新LuckPerms挂钩方法

--- src/main/java/cn/originmc/plugins/origincore/OriginCore.java
+++ src/main/java/cn/originmc/plugins/origincore/OriginCore.java
@@ -6,7 +6,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class OriginCore extends JavaPlugin {
     private static JavaPlugin instance;
     private static Sender sender;
-    private static final String VERSION = "1.0.1";
+    private static final String VERSION = "1.0.2";
     public static JavaPlugin getInstance() {
         return instance;
     }
