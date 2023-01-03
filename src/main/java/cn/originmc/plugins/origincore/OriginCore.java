2e9cc584fa01900579b3d1fb2fd39f8ee086d881
Yeqi99 <72844744+Yeqi99@users.noreply.github.com>
2023-01-03 09:32:37 +0800

    版本号更新

--- src/main/java/cn/originmc/plugins/origincore/OriginCore.java
+++ src/main/java/cn/originmc/plugins/origincore/OriginCore.java
@@ -6,7 +6,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class OriginCore extends JavaPlugin {
     private static JavaPlugin instance;
     private static Sender sender;
-    private static final String VERSION = "1.0.0";
+    private static final String VERSION = "1.0.1";
     public static JavaPlugin getInstance() {
         return instance;
     }
