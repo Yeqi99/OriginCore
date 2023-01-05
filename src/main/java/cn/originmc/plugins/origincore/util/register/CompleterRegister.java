6a636549c1c07baa2929de4998f319a27b68e5f8
Yeqi99 <72844744+Yeqi99@users.noreply.github.com>
2023-01-05 18:41:50 +0800

    指令补全注册器

--- src/main/java/cn/originmc/plugins/origincore/util/register/CompleterRegister.java
+++ src/main/java/cn/originmc/plugins/origincore/util/register/CompleterRegister.java
@@ -1,10 +1,16 @@
 package cn.originmc.plugins.origincore.util.register;
 
+import org.bukkit.command.TabCompleter;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CompleterRegister {
-    public static void register(JavaPlugin plugin, Listener listener){
-        plugin.getServer().getPluginManager().registerEvents(listener,plugin);
+    public static boolean register(JavaPlugin plugin,TabCompleter tabCompleter,String command){
+        if(plugin.getServer().getPluginCommand(command)==null){
+            return false;
+        }else{
+            plugin.getServer().getPluginCommand(command).setTabCompleter(tabCompleter);
+            return true;
+        }
     }
 }
